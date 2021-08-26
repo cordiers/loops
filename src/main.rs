7a1df65a8149318a9ae6eb8d43ab1c6bd1eecc50
Sylvain Cordier <sylvaincordier@MacBook-Pro-de-Sylvain-2.local>
2021-08-26 17:35:58 +0200

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 fn main() {
-  let a = [10, 20, 30, 40];
+    let s = String::from("hello");  // s comes into scope
+    takes_ownership(s);             // s's value moves into the function...
+                                       // ... and so is no longer valid here
+    let x = 5;                      // x comes into scope
+    makes_copy(x);                  // x would move into the function,
+    
+    println!("Value of x : {}", x);// but i32 is Copy, so it's okay to
+                                       // still use x afterward
+} // Here, x goes out of scope, then s. But because s's value was moved,
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+// memory is freed.
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+   println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
+   
 
-  let mut index = 0;
-
-  while index < 4 {
-      println!(" number : {}", a[index]);
-      index = index + 1;
-  }
-}
